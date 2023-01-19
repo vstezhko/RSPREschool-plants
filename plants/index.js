53f0502e003f82a9789a18ee1fe775e4f1cfe8fb
vika <viktoriastezhko@gmail.com>
2023-01-19 13:03:16 +0300

    feat: add service card blur

--- plants/index.js
+++ plants/index.js
@@ -1,12 +1,12 @@
 console.log("все требования учтены")
 
 window.onload = function() {
+    /// menu block
     const burger = document.querySelector('.burger')
     const nav = document.querySelector('nav')
     const body = document.querySelector('body')
     const main = document.querySelector('main')
     const navItems = document.querySelectorAll('nav ul li')
-
     const handleMenu = (type) => {
         if(window.innerWidth < 721) {
             if (type === 'toggle') {
@@ -32,5 +32,46 @@ window.onload = function() {
     main.addEventListener('click', ()=>{
         handleMenu()
     })
+
+    // service block
+
+    const serviceButtons = document.querySelectorAll('.service .button')
+    const cards = document.querySelectorAll('.service-cards .card')
+
+    let activeService = []
+
+    const setThirdDisabled = () => {
+        if (activeService.length === 2) {
+            serviceButtons.forEach(button => {
+                if (!activeService.includes(button.attributes['data-service'].value)) {
+                    button.disabled = true
+                }
+            })
+        }
+    }
+    const removeThirdDisabled = () => {
+        if (activeService.length < 2) {
+            serviceButtons.forEach(button => {
+                    button.disabled = false
+
+            })
+        }
+    }
+
+    serviceButtons.forEach(btn => btn.addEventListener('click', (e)=>{
+        if (!activeService.includes(e.target.attributes['data-service'].value) && activeService.length<2 ) {
+            btn.classList.toggle('active')
+            activeService.push(e.target.attributes['data-service'].value)
+        } else if (activeService.includes(e.target.attributes['data-service'].value) && activeService.length<3 ){
+            btn.classList.toggle('active')
+            activeService = activeService.filter(i => i !== e.target.attributes['data-service'].value)
+        }
+
+        cards.forEach(card => {
+            activeService.includes(card.attributes['data-service'].value) ? card.classList.add('blur') : card.classList.remove('blur')
+        })
+        removeThirdDisabled()
+        setThirdDisabled()
+    }))
 
 };
