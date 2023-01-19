8c653bdf6b7e4122a15d0a4f34c15e2bed262c01
vika <viktoriastezhko@gmail.com>
2023-01-19 09:58:57 +0300

    feat: refactor handleMenu

--- plants/index.js
+++ plants/index.js
@@ -5,30 +5,32 @@ window.onload = function() {
     const nav = document.querySelector('nav')
     const body = document.querySelector('body')
     const main = document.querySelector('main')
-
     const navItems = document.querySelectorAll('nav ul li')
-    burger.addEventListener('click', ()=> {
+
+    const handleMenu = (type) => {
         if(window.innerWidth < 721) {
-            burger.classList.toggle('active')
-            nav.classList.toggle('active')
-            body.classList.toggle('lock')
+            if (type === 'toggle') {
+                burger.classList.toggle('active')
+                nav.classList.toggle('active')
+                body.classList.toggle('lock')
+            } else {
+                burger.classList.remove('active')
+                nav.classList.remove('active')
+                body.classList.remove('lock')
+            }
         }
+    }
+
+    burger.addEventListener('click', ()=> {
+        handleMenu('toggle')
     })
 
     navItems.forEach(item => item.addEventListener('click', ()=>{
-        if(window.innerWidth < 721) {
-            burger.classList.toggle('active')
-            nav.classList.toggle('active')
-            body.classList.toggle('lock')
-        }
+        handleMenu()
     }))
 
     main.addEventListener('click', ()=>{
-        if(window.innerWidth < 721) {
-            burger.classList.remove('active')
-            nav.classList.remove('active')
-            body.classList.remove('lock')
-        }
+        handleMenu()
     })
 
 };
